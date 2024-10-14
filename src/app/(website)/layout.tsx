649de36a576fe8faa9b08e02857f3dffc80f7824
一纸忘忧 <i@ikxin.com>
2024-10-14 20:05:09 +0800

    build: migrate to yarn

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -14,6 +14,7 @@ import 'styles/variables.css';
 import 'styles/index.css';
 import '@umami/react-zen/dist/zen.css';
 import '@umami/react-zen/dist/styles.css';
+import 'react-basics/dist/styles.css';
 
 export default function RootLayout({ children }) {
   return (
