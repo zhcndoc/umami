15b9bc2a812ed0b206c3e7eeb8b176e5a06fe2ae
Mike Cao <moocao@gmail.com>
2025-11-05 01:30:11 -0800

    Added asset prefix.

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  assetPrefix: '/docs',
   reactStrictMode: true,
   env: {
     trackerId: process.env.TRACKER_ID || '',
